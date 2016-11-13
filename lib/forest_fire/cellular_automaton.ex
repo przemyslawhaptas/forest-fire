3c7134111fb92cff71f7eed4ccc0f92c5dca1d08
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-11-13 16:42:01 +0100

    [cellular-automaton] Change board implementation from lists to MapSets

--- lib/forest_fire/cellular_automaton.ex
+++ lib/forest_fire/cellular_automaton.ex
@@ -1,59 +1,52 @@
 defmodule ForestFire.CellularAutomaton do
   def burn_trees_down({ trees, burning_trees, empty_cells }) do
-    { trees, [],  (empty_cells ++ burning_trees) |> Enum.sort }
+    { trees, %MapSet{},  MapSet.union(empty_cells, burning_trees) }
   end
 
   def spread_fire({ trees, burning_trees, empty_cells }) do
-    newly_ingnited =
+    newly_ingnited_trees =
       burning_trees
       |> adjacent_cells
-      |> affected_trees(trees)
+      |> MapSet.intersection(trees)
 
-    trees_after = (trees -- newly_ingnited) |> Enum.sort
-    burning_trees_after = (burning_trees ++ newly_ingnited) |> Enum.sort
+    unaffected_trees = MapSet.difference(trees, newly_ingnited_trees)
+    all_burning_trees = MapSet.union(burning_trees, newly_ingnited_trees)
 
-    { trees_after, burning_trees_after, empty_cells }
+    { unaffected_trees, all_burning_trees, empty_cells }
   end
 
-  def adjacent_cells(cells) when cells |> is_list do
-    all_adjacent_cells =
-      cells
-      |> Enum.reduce([], fn(cell, adjacent_cells_acc) ->
-          adjacent_cells(cell) ++ adjacent_cells_acc end)
-      |> Enum.uniq
+  def strike_lightnings({ trees, burning_trees, e_c }, p_lightning_prob) do
+    struck_trees =
+      for tree <- trees, :rand.uniform(100) <= p_lightning_prob,
+      into: %MapSet{},
+      do: tree
 
-    all_adjacent_cells -- cells
+    { MapSet.difference(trees, struck_trees),
+      MapSet.union(burning_trees, struck_trees),
+      e_c }
+  end
+
+  def grow_trees({ trees, b_t, empty_cells }, f_growth_prob) do
+    grown_trees =
+      for empty_cell <- empty_cells, :rand.uniform(100) <= f_growth_prob,
+      into: %MapSet{},
+      do: empty_cell
+
+    { MapSet.union(trees, grown_trees),
+      b_t,
+      MapSet.difference(empty_cells, grown_trees) }
+  end
+
+  def adjacent_cells(%MapSet{} = cells) do
+      cells
+      |> Enum.reduce(%MapSet{}, fn(cell, adjacent_cells_acc) ->
+          MapSet.union(adjacent_cells(cell), adjacent_cells_acc) end)
+      |> MapSet.difference(cells)
   end
   def adjacent_cells({ x, y }) do
     for x_cord <- (x - 1)..(x + 1),
         y_cord <- (y - 1)..(y + 1),
-        !(x_cord == x && y_cord == y), do: { x_cord, y_cord }
-  end
-
-  def affected_trees(cells_in_fire_range, trees) do
-    intersect_lists(cells_in_fire_range, trees)
-  end
-
-  def strike_lightnings({ trees, burning_trees, e_c }, p_lightning_prob) do
-    struck_trees = for tree <- trees,
-                       :rand.uniform(100) <= p_lightning_prob, do: tree
-
-    { trees -- struck_trees, burning_trees ++ struck_trees, e_c }
-  end
-
-  def grow_trees({ trees, b_t, empty_cells }, f_growth_prob) do
-    grown_trees = for empty_cell <- empty_cells,
-                      :rand.uniform(100) <= f_growth_prob, do: empty_cell
-
-    { trees ++ grown_trees, b_t, empty_cells -- grown_trees }
-  end
-
-  defp intersect_lists(list1, list2) do
-    list1_uniq = Enum.uniq(list1)
-    list2_uniq = Enum.uniq(list2)
-
-    diff = list1_uniq -- list2_uniq
-    list1_uniq -- diff
+        !(x_cord == x && y_cord == y), into: %MapSet{}, do: { x_cord, y_cord }
   end
 
   # def next_turn({ board, params }) do
@@ -61,13 +54,5 @@ defmodule ForestFire.CellularAutomaton do
   #   board2 = spread_fire(board)
   #   board3 = strike_a_lightning(board, p_lightning_prob)
   #   board4 = grow_trees(board, f_growth_prob)
-  # end
-  #
-  # def strike_a_lightning(state) do
-  #   state
-  # end
-  #
-  # def grow_trees(state) do
-  #   state
   # end
 end
--- spec/forest_fire/cellular_automaton_spec.exs
+++ spec/forest_fire/cellular_automaton_spec.exs
@@ -15,9 +15,10 @@ defmodule ForestFire.CellularAutomatonSpec do
        -1 0 1
   """
 
-  let :trees, do:         [{ -1, -1 }, { 0, -2 }, { 0, -1 }, { 1, 2 }]
-  let :burning_trees, do: [{ -1, 0 }, { 0, 0 }, { 1, -1 }]
-  let :empty_cells, do: [{ 0, 1 }, { 1, 0 }, { 1, 1 }]
+  let :trees, do:         MapSet.new([{ -1, -1 }, { 0, -2 }, { 0, -1 }, { 1, 2 }])
+  let :burning_trees, do: MapSet.new([{ -1, 0 }, { 0, 0 }, { 1, -1 }])
+  let :empty_cells, do:   MapSet.new([{ 0, 1 }, { 1, 0 }, { 1, 1 }])
+
   let :board, do: { trees, burning_trees, empty_cells }
 
   describe "burn_trees_down/1" do
@@ -25,52 +26,38 @@ defmodule ForestFire.CellularAutomatonSpec do
 
     it do: is_expected.to eq(
       { trees,
-        [],
-        [{ -1, 0 }, { 0, 0 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }] |> Enum.sort })
+        %MapSet{},
+        MapSet.new([{ -1, 0 }, { 0, 0 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }]) })
   end
 
   describe "spread_fire/1" do
     subject(described_module().spread_fire(board))
 
-    let :trees_after, do: [{ 1, 2 }]
-    let :burning_trees_after, do:
-      [{ -1, -1 }, { 0, -2 }, { 0, -1 }, { -1, 0 }, { 0, 0 }, { 1, -1 }]
+    let :unaffected_trees, do: MapSet.new([{ 1, 2 }])
+    let :all_burning_trees, do: MapSet.new(
+      [{ -1, -1 }, { 0, -2 }, { 0, -1 }, { -1, 0 }, { 0, 0 }, { 1, -1 }])
 
-    it do
-      { t, b_t, e_c } = subject
-
-      expect({ t |> Enum.sort, b_t |> Enum.sort, e_c })
-        .to eq({ trees_after |> Enum.sort, burning_trees_after |> Enum.sort, empty_cells })
-    end
+    it do: is_expected.to eq({ unaffected_trees, all_burning_trees, empty_cells })
   end
 
   describe "adjacent_cells/1" do
-    describe "when argument is a cell" do
-      subject(described_module().adjacent_cells({ 0, 0 }) |> Enum.sort)
+    describe "when argument is a set of cells" do
+      subject(described_module().adjacent_cells(MapSet.new([{ 0, 0 }, { 1, 0 }])))
 
-      it do: is_expected.to eq([
-        { -1, -1 }, { -1, 0 }, { -1, 1 },
-        { 0, -1 }, { 0, 1 },
-        { 1, -1 }, { 1, 0 }, { 1, 1 }]
-        |> Enum.sort)
-    end
-    describe "when argument is a list of cells" do
-      subject(described_module().adjacent_cells([{ 0, 0 }, { 1, 0 }]) |> Enum.sort)
-
-      it do: is_expected.to eq([
+      it do: is_expected.to eq(MapSet.new([
         { -1, -1 }, { -1, 0 }, { -1, 1 },
         { 0, -1 }, { 0, 1 },
         { 1, -1 }, { 1, 1 },
-        { 2, -1 }, { 2, 0 }, { 2, 1 }]
-        |> Enum.sort)
+        { 2, -1 }, { 2, 0 }, { 2, 1 }]))
     end
-  end
+    describe "when argument is a cell" do
+      subject(described_module().adjacent_cells({ 0, 0 }))
 
-  describe "affected_trees/1" do
-    subject(described_module().affected_trees(cells_in_fire_range, trees) |> Enum.sort)
-
-    let :cells_in_fire_range, do: [{ -1, -1 }, { 0, -2 }, { -1, 0 }, { 1, 1 }]
-    it do: is_expected.to eq([{ -1, -1 }, { 0, -2 }] |> Enum.sort)
+      it do: is_expected.to eq(MapSet.new([
+        { -1, -1 }, { -1, 0 }, { -1, 1 },
+        { 0, -1 }, { 0, 1 },
+        { 1, -1 }, { 1, 0 }, { 1, 1 }]))
+    end
   end
 
   describe "strike_lightnings/2" do
@@ -80,7 +67,10 @@ defmodule ForestFire.CellularAutomatonSpec do
 
       let :p_lightning_prob, do: 100
 
-      it do: is_expected.to eq({ [], burning_trees ++ trees, empty_cells })
+      it do: is_expected.to eq(
+        { %MapSet{},
+          MapSet.union(burning_trees, trees),
+          empty_cells })
     end
 
     describe "when there is 0% chance of lightning strike" do
@@ -99,7 +89,10 @@ defmodule ForestFire.CellularAutomatonSpec do
 
       let :f_growth_prob, do: 100
 
-      it do: is_expected.to eq({ trees ++ empty_cells, burning_trees, [] })
+      it do: is_expected.to eq(
+        { MapSet.union(trees, empty_cells),
+          burning_trees,
+          %MapSet{} })
     end
 
     describe "when there is 0% chance of growing a tree" do
